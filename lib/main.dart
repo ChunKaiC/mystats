9055c02ca191f60ae808ec5271439d5d74112ba9
ChunKaiC <baj49jab@gmail.com>
2023-06-03 02:24:49 -0400

    route and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:mystats/components/navbar.dart';
+import 'package:mystats/views/home_view.dart';
 import 'package:mystats/views/landing_view.dart';
 import 'package:mystats/views/login_view.dart';
+import 'package:mystats/views/sign_up_view.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -24,17 +28,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle.light,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => NavbarProvider())],
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+          ),
         ),
+        initialRoute: '/landing_view',
+        routes: {
+          '/landing_view': (context) => const LandingView(),
+          '/login_view': (context) => const LoginView(),
+          '/sign_up_view': (context) => const SignUpView(),
+          '/home_view': (context) => HomeView(),
+        },
       ),
-      initialRoute: '/landing_view',
-      routes: {
-        '/landing_view': (context) => const LandingView(),
-        '/login_view': (context) => const LoginView(),
-      },
     );
   }
 }
